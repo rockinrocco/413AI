bb2679408dc9cfab198d0db1d35efbd4b8847aab
Matt Rocco <roccoma@rose-hulman.edu>
2014-10-27 09:37:05 -0400

    Update Main.java

--- src/files/Main.java
+++ src/files/Main.java
@@ -3,38 +3,98 @@ package files;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.TreeMap;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) throws IOException {
 
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("Please enter the fileName");
-		String fileName = scanner.nextLine();
-		System.out.println("Enter Algorithm Number \n 1 - BM25 "
-				+ "\n 2 - Skip bi-Grams \n 3 - n-grams \n 4 - Passge Term Matching \n 5 - Textual Alignment");
-		int algorithmNumber = scanner.nextInt();
-		System.out.println("Analyzing " + fileName + " with algorithm " + algorithmNumber);	
-		
-		fileUtil fileUtil = new fileUtil(fileName);
-		String fileContents = fileUtil.fileText;
-		System.out.println(fileContents);
-	}
+        fileUtil fileUtil = new fileUtil();
 
-	private String readFile( String file ) throws IOException {
-	    BufferedReader reader = new BufferedReader( new FileReader (file));
-	    String         line = null;
-	    StringBuilder  stringBuilder = new StringBuilder();
-	    String         ls = System.getProperty("line.separator");
+        String[] presidentFiles = new String[] {
+            "AbrahamLincoln.txt", "AndrewJackson.txt", "AndrewJohnson.txt", "BarackObama.txt", "BenjaminHarrison.txt", "BillClinton.txt", "CalvinCoolidge.txt", "ChesterArthur.txt", "DwightEisenhower.txt", "FranklinDRoosevelt.txt", "FranklinPierce.txt", "GeorgeBush.txt", "GeorgeWashington.txt", "GeorgeWBush.txt", "GeraldFord.txt", "GroverCleveland.txt", "HarryTruman.txt", "HerbertHoover.txt", "JamesBuchanan.txt", "JamesGarfield.txt", "JamesMadison.txt", "JamesMonroe.txt", "JamesPolk.txt", "JimmyCarter.txt", "JohnAdams.txt", "JohnKennedy.txt", "JohnQuincyAdams.txt", "JohnTyler.txt", "LyndonJohnson.txt", "MartinVanBuren.txt", "MillardFillmore.txt", "RichardNixon.txt", "RonaldReagan.txt", "RutherfordHayes.txt", "TheodoreRoosevelt.txt", "ThomasJefferson.txt", "UlyssesGrant.txt", "WarrenHarding.txt", "WilliamHenryHarrison.txt", "WilliamMcKinley.txt", "WilliamTaft.txt", "WoodrowWilson.txt", "ZacharyTaylor.txt"
+        };
+        String[] queryTerms = new String[] {
+            "adams", "lincoln", "president", "assassinated president", "great president", "first president", "civil war president"
+        };
+        Map < String, String[] > fileWords = new HashMap < String, String[] > ();
+        //Query Terms -> Query Words -> filenames -> Algorithms -> Results
 
-	    while( ( line = reader.readLine() ) != null ) {
-	        stringBuilder.append( line );
-	        stringBuilder.append( ls );
-	    }
+        int wordCount = 0;
+        //map with all the files words. map(fileName, words[]);
+        for (String names: presidentFiles) {
 
-	    return stringBuilder.toString();
-	}
+            String contents = fileUtil.readFile(names);
+            String[] words = fileUtil.format(contents);
+            fileWords.put(names, words);
+            wordCount += words.length;
+        }
+        double averageLength = wordCount / presidentFiles.length;
 
-}
+      Map < String, Integer > queryMatches = new HashMap < String, Integer > ();
+        
+        for (String query: queryTerms) {
+          String[] queryWords = query.split(" ");
+          
+        	for (String qWord: queryWords) {
+
+        		for (String names: presidentFiles) {
+        	  String[] words = fileWords.get(names);
+            int matches = fileUtil.countMatches(qWord, words);
+            queryMatches.put(names + ":" + qWord, matches);
+          //  System.out.println(names + ":" + qWord +":" + matches);
+        		}
+        	}
+        	}
+        }
+    }
+
+        	//failed bm25 attempt. Will scrap and start over.
+//            String[] queryWords = query.split(" ");
+//            int count = 0;
+//            Map < String, Double > queryScores = new HashMap < String, Double > ();
+//            for (String qWord: queryWords) {
+//                //individual docScores for each query word.
+//                Map < String, Double > docScores = new HashMap < String, Double > ();
+//
+//                int inDocuments = 0;
+//                for (String names: presidentFiles) {
+//                    count++;
+//                    String[] words = fileWords.get(names);
+//                    int matches = fileUtil.countMatches(qWord, words);
+//                    if (matches != 0) {
+//                        inDocuments++;
+//                    }
+//                    //http://en.wikipedia.org/wiki/Okapi_BM25
+//                    docScores.put(names, ((matches + 2.5) / (matches + 1.5 * (1 - 0.5 + 0.5 * (words.length / averageLength)))));
+//                }
+//
+//                double IDF = (presidentFiles.length - inDocuments + 0.5) / (inDocuments + 0.5);
+//                for (String names: presidentFiles) {
+//                    double docScore = IDF * docScores.get(names);
+//                    docScores.put(names, docScore);
+//                    double current = 0;
+//                    if (queryScores.get(names) == null) {
+//                        current = 0;
+//                    } else
+//                        current = queryScores.get(names);
+//
+//                    queryScores.put(names, current + docScore);
+//                }
+//            }
+//            TreeMap<Double, String> topScores = new TreeMap<Double, String>();
+//            System.out.println("Query: " + query);
+//
+//            for (String names: presidentFiles) {
+//            	//find the top ten.
+//            	System.out.println(names + "..... Score: " + queryScores.get(names));
+//
+//            }
+//
+//        }
+//    }
+//}
